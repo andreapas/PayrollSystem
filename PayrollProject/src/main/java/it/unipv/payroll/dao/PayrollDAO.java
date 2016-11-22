db2120b84899c57ca2b71968d4baa9f24eb5f2c5
Andrea Pasquali <andrea_pasquali@hotmail.it>
2016-11-22 10:29:35 +0100

    second commit

---
+++ PayrollProject/src/main/java/it/unipv/payroll/dao/PayrollDAO.java
@@ -0,0 +1,28 @@
+package it.unipv.payroll.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import it.unipv.payroll.model.Payroll;
+
+@Stateless
+public class PayrollDAO {
+
+	@PersistenceContext
+	EntityManager em;
+	
+	public List<Payroll> findAll() {
+		
+		// JPQL Query
+		//perché la select ha un "p" eppure funziona? perché passo il PhoneBook.class? È per ottenere @table e l'entità Phonebook?
+		List<Payroll> phoneBooks =
+				em.createQuery("select p from Payroll p", Payroll.class)
+				.getResultList();
+		
+		return phoneBooks;
+	}
+	
+}
---
+++ PayrollProject/src/main/java/it/unipv/payroll/model/Payroll.java
@@ -0,0 +1,34 @@
+package it.unipv.payroll.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="payroll")
+public class Payroll {
+
+	@Id
+	private int id;
+	
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Payroll [message=" + message + "]";
+	}
+	
+	
+}
