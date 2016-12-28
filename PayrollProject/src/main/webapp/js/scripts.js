6396aec544ce94ffebe27e9f60d1ab04c8ede062
Davide <Davide@macbookdidavide.lan>
2016-12-28 19:25:17 +0100

    Nice Refactoring of the view
    All the show/hide functions are in the javascript
    
    Signed-off-by: Davide <Davide@macbookdidavide.lan>

--- PayrollProject/src/main/webapp/js/scripts.js
+++ PayrollProject/src/main/webapp/js/scripts.js
@@ -2,6 +2,25 @@ function showElementsOfRole(role) {
 	var roleClass = role.toLowerCase();
 	var divs = document.getElementsByClassName(roleClass);
 	for(var i=0; i<divs.length; i++) { 
-	  divs[i].style.display='block'
+		divs[i].style.display='block';
 	}
+}
+
+function hideDivsByClass(divClass) {
+	var divs = document.getElementsByClassName(divClass);
+	for(var i=0; i<divs.length; i++) { 
+		divs[i].style.display='none';
+	}
+}
+
+function showDivsByClass(divClass) {
+	var divs = document.getElementsByClassName(divClass);
+	for(var i=0; i<divs.length; i++) { 
+		divs[i].style.display='block';
+	}
+}
+
+function showOnlyDivByClass(divClass, excludeClass) {
+	hideDivsByClass(excludeClass);
+	showDivsByClass(divClass);
 }
