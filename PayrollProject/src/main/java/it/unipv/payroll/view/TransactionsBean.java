4ac30077651c591c30784cd4970ca2a1f2d28cf3
Davide <Davide@macbookdidavide.lan>
2017-01-11 20:00:09 +0100

    Insert Sale for the monthly index, improved
    Signed-off-by: Davide <Davide@macbookdidavide.lan>

--- PayrollProject/src/main/java/it/unipv/payroll/view/TransactionsBean.java
+++ PayrollProject/src/main/java/it/unipv/payroll/view/TransactionsBean.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.swing.plaf.synth.SynthSeparatorUI;
 
 import it.unipv.payroll.controller.TransactionsController;
 import it.unipv.payroll.model.Employee;
@@ -18,7 +19,8 @@ public class TransactionsBean implements Serializable {
 	@Inject TransactionsController tController;
 
 	private Transactions transaction;
-	
+	private int id;
+
 	@PostConstruct
 	public void init() {
 		transaction = new Transactions();
@@ -26,6 +28,16 @@ public class TransactionsBean implements Serializable {
 
 	public String addTransaction(Employee employee){
 		transaction.setEmployee(employee);
+		System.out.println(employee.getRole());
+		if (employee.getRole().equals("Monthly")) {
+			transaction.setInfo("Sale ID="+id);
+		}
+		if (employee.getRole().equals("Weekly")) {
+			transaction.setInfo("Extra hours");
+		}
+		else {
+			System.out.println("Errrrore!");
+		}
 		String answer = tController.add(transaction);
 		return answer;
 	}
@@ -35,5 +47,12 @@ public class TransactionsBean implements Serializable {
 	}
 	public void setTransaction(Transactions transaction) {
 		this.transaction = transaction;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
 	}
 }
